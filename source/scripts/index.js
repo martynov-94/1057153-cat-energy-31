f482c447952e600151bd660c5a5939c961d35fe1
martynov-94 <31200896+martynov-94@users.noreply.github.com>
2026-02-15 12:19:55 +0300

    Продолжает стилизацию index-mobile

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,8 @@
 /* в этот файл добавляет скрипты*/
+const menuToggleButton = document.querySelector('.navigation__menu-toggle');
+const navigation = document.querySelector('.navigation');
+
+
+menuToggleButton.addEventListener('click', () => {
+  navigation.classList.toggle('navigation--open');
+});
